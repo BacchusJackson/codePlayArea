5b2ba22075a7393b2ae328c2bc6ef8e6193a01af
Bacchus Jackson <bjjackson32@gmail.com>
2018-12-08 19:33:21 -0500

    added new file

---
+++ python/LEDRGB2.py
@@ -0,0 +1,43 @@
+#Program asks for user input to determine color to shine.
+
+import RPi.GPIO as GPIO
+
+pinRed = 11   #Set to appropriate GPIO
+pinGreen = 15 #Should be set in the 
+pinBlue = 13  #GPIO.BOARD format
+
+# def blink(pin):
+#     GPIO.setmode(GPIO.BOARD)
+    
+#     GPIO.setup(pin, GPIO.OUT)
+#     GPIO.output(pin, GPIO.HIGH)
+    
+# def turnOff(pin):
+#     GPIO.setmode(GPIO.BOARD)
+#     GPIO.setup(pin, GPIO.OUT)
+#     GPIO.output(pin, GPIO.LOW)
+
+class Light():
+    def __init__(self):
+        self.status = ''
+        self.colors = {
+            'red':[pinRed], 'blue':[pinBlue], 
+            'green':[pinGreen], 'yellow':[pinRed, pinGreen]
+            }
+
+    def setColor(self, color):
+        GPIO.setmode(GPIO.BOARD)
+        for pin in self.colors[color]:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.HIGH)
+
+    def off(self):
+        for color in self.colors:
+            for pin in self.colors[color]:
+                GPIO.setmode(GPIO.BOARD)
+                GPIO.setup(pin, GPIO.OUT)
+                GPIO.output(pin, GPIO.LOW)
+        self.status = 'off'
+
+light1 = Light()
+light1.setColor('blue')
---
+++ python/test.py
@@ -0,0 +1,6 @@
+colors = {'red':[11, 12], 'blue':[13, 14]}
+
+for color in colors:
+    for pin in colors[color]:
+        print(pin)
+
