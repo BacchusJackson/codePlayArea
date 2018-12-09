c93e8bc3e8af4cb9040074e9e310b5e4cd425d09
Bacchus Jackson <bjjackson32@gmail.com>
2018-12-09 00:54:32 -0500

    IDK ANYMORE

--- python/wtfLED.py
+++ python/wtfLED.py
@@ -18,14 +18,17 @@ r.start(0)
 g.start(0)
 b.start(0)
 
-print('started set color')
-r.ChangeDutyCycle(float(50))
-g.ChangeDutyCycle(float(50))
-b.ChangeDutyCycle(float(50))
-time.sleep(10)
+def setColor(red, green, blue):
+    print('started set color')
+    r.ChangeDutyCycle(float(red))
+    g.ChangeDutyCycle(float(green))
+    b.ChangeDutyCycle(float(blue))
+    time.sleep(.04)
 
 usrInput = ""
 
 while usrInput != '0':
     usrInput = input('--> ')
+    if usrInput == '1':
+        setColor(50, 50, 50)
 
